2ac58bc23775329835e07d4e7e736d6793d7e292
Jonas Habelmann <jonashabelmann@gmail.com>
2018-12-12 14:10:32 +0100

    MySQL Connection

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,19 +1,26 @@
 package gui;
 
 import gui.kunde.KundeControl;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
-	@Override
-	public void start(Stage primaryStage) {
-		new KundeControl(primaryStage);
-	}	
-	
-	public static void main(String[] args) 
-	{
-		launch(args);
-	}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        new KundeControl(primaryStage);
+
+        MySQLAccess.init("localhost","root", "", "bautraegerfx");
+
+        try {
+            MySQLAccess.GetInstance().getConnection().prepareStatement("SELECT * FROM kunde");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
