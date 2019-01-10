582701c6f2d3b2f78e890d6444ee647b3d8a39df
Jonas Habelmann <jonashabelmann@gmail.com>
2019-01-10 18:01:46 +0100

    fixing db errors

--- src/main/java/db/DBConnector.java
+++ src/main/java/db/DBConnector.java
@@ -31,7 +31,7 @@ public class DBConnector {
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
             String sql;
-            sql = "SELECT Dachgeschoss FROM Bauplan WHERE Plannummer=" + planNummer;
+            sql = "SELECT Dachgeschoss FROM bauplan WHERE Plannummer=" + planNummer;
             ResultSet rs = stmt.executeQuery(sql);
 
             while (rs.next()) {
@@ -71,7 +71,7 @@ public class DBConnector {
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
             String sql;
-            sql = "INSERT INTO Kunden (Vorname, Nachname, Telefonnummer, E-Mail-Adresse) " +
+            sql = "INSERT INTO kunden (Vorname, Nachname, Telefonnummer, E-Mail-Adresse) " +
                     "VALUES(" + vorname + ", " + nachname + ", " + telefonnummer + ", " + email + ");";
             ResultSet rs = stmt.executeQuery(sql);
 
@@ -111,7 +111,7 @@ public class DBConnector {
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
             String sql;
-            sql = "SELECT * FROM Kunden WHERE Vorname LIKE '%" + vorname + "%' OR Nachname LIKE '%" + nachname
+            sql = "SELECT * FROM kunden WHERE Vorname LIKE '%" + vorname + "%' OR Nachname LIKE '%" + nachname
                     + "%' OR Telefonnummer LIKE '%" + telefonnummer + "%' OR E-Mail-Adresse LIKE '%" + email + "%';";
             ResultSet rs = stmt.executeQuery(sql);
 
@@ -162,7 +162,7 @@ public class DBConnector {
             con = DriverManager.getConnection(db_url, user, pass);
 
             //Datenbank hat noch keine Tabelle/Spalte fuer Bilder!
-            String sql = "SELECT Bild FROM Bauplan WHERE Plannummer = ?";
+            String sql = "SELECT Bild FROM bauplan WHERE Plannummer = ?";
 
             pstmt = con.prepareStatement(sql);
             pstmt.setInt(1, id);
